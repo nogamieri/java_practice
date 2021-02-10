a54aca8be70e7ed128723bdcb87e93c9c6fd11ed
恵里 <komeuma726@gmail.com>
2021-02-10 22:44:01 +0900

    day12

--- 20210209/src/Example.java
+++ 20210209/src/Example.java
@@ -1,4 +1,16 @@
-
+class Student {
+	String name;
+	int age;
+	
+	Student(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+	void printInfo() {
+		System.out.println("私の名前は" + this.name + "です。" + this.age + "歳です。");
+	}
+}
 public class Example {
 
 }
